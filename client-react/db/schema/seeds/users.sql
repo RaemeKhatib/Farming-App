4a9096b59762034ee20e4179d8547f20c52a084d
Raeme Khatib <raeme.khatib@telus.com>
2023-02-03 16:19:54 -0700

    update seed folder

--- client-react/db/schema/seeds/users.sql
+++
@@ -1,3 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users (email, admin, password) VALUES ('b@b.ca', false, 'password');
-INSERT INTO users (email, admin, password) VALUES ('p@p.ca', true, 'password');
