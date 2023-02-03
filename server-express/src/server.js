cab030b4b44abbac70e082bba77e3f33e1360128
Raeme Khatib <raeme.khatib@telus.com>
2023-02-03 16:10:03 -0700

    remove morgan

--- server-express/src/server.js
+++ server-express/src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require("express");
 const path = require('path');
-const morgan = require('morgan')
 
 
 const app = express();
@@ -12,7 +11,6 @@ const cwd = process.cwd();
 // const public = path.join(cwd, '..', 'public');
 console.log("public dir: ", cwd + "/public");
 app.use(express.static(cwd + "/public"));
-app.use(morgan("dev"))
 
 
 // Do Not make a route for "/" or it will override public
