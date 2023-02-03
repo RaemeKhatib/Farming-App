ab4e469b1f9a8506d4e2b6112ed2a7b984a4e408
Krista Kourtessis <krista.kourtessis@telus.com>
2023-02-03 15:17:44 -0800

    update seeds folder

---
+++ client-react/db/seeds/users.sql
@@ -0,0 +1,3 @@
+-- Users table seeds here (Example)
+INSERT INTO users (email, admin, password) VALUES ('b@b.ca', false, 'password');
+INSERT INTO users (email, admin, password) VALUES ('p@p.ca', true, 'password');
