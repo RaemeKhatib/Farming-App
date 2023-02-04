13f9e724b0375442bc9aed4528fd599b4251f9f5
Raeme Khatib <raeme.khatib@telus.com>
2023-02-03 17:39:55 -0700

    broken code that is supposed to have an axios request for test for routes for queuries

--- client-react/src/Login.js
+++ client-react/src/Login.js
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import React, { useState } from "react";
+import axios from 'axios';
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -11,6 +12,15 @@ export default function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Form submitted with value: ", email);
+  };
+
+  const handleUsers= (event) => {
+    event.preventDefault();
+    axios.get("/test")
+    .then(users => {
+      console.log("Form submitted with value: ", users);
+    })
+    
   };
 
   return (
@@ -28,7 +38,10 @@ export default function Login() {
         <TextField id="outlined-basic" label="Email" variant="outlined" value={email} onChange={(event) => setEmail(event.target.value)} />
         <TextField id="outlined-basic" label="Password" variant="outlined" value={password} onChange={(event) => setPassword(event.target.value)} />
         <Button style={{ backgroundColor: 'red' }} variant="submit" onClick={handleSubmit}>Submit</Button>
+        
       </Box>
+
+      <Button style={{ backgroundColor: 'blue' }} variant="submit" onClick={handleUsers}>Users</Button>
 
     </>
 
--- server-express/src/server.js
+++ server-express/src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+
 const express = require("express");
 const path = require('path');
+const { getUsers } = require('../db/queries/test');
 
 
 const app = express();
@@ -8,21 +10,32 @@ const PORT = process.env.PORT || 8080;
 
 // serve static files from ../build (needed for React)
 const cwd = process.cwd();
-// const public = path.join(cwd, '..', 'public');
-console.log("public dir: ", cwd + "/public");
-app.use(express.static(cwd + "/public"));
+
+// app.use(express.static(cwd + "/public"));
 
 
 // Do Not make a route for "/" or it will override public
 
 app.get("/api/status", (req, res) => {
-  res.json({version: "1.01"});
+  res.json({ version: "1.01" });
 });
 
 app.use(function(req, res) {
   res.status(404);
 });
 
+
+
+app.get("/test", (req, res) => {
+  console.log("I NEVER KNOW WHAT IM DOING DAVID")
+  getUsers()
+  
+    .then(data => {
+      console.log("data", data)
+     res.json(data);
+    });
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
